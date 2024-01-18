1970909e67ddd26cd7f584a845b93b392d1e59b8
JulesBernardBdeB <1265963@bdeb.qc.ca>
2024-01-18 15:30:07 -0500

    linked form with servlet, added error message

--- src/main/java/com/example/devoira18/CalculatorServlet.java
+++ src/main/java/com/example/devoira18/CalculatorServlet.java
@@ -2,15 +2,29 @@ package com.example.devoira18;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "CalculatorServlet", value = "/InterestRateCalculator")
 public class CalculatorServlet extends HttpServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
+        String Principal = request.getParameter("principal");
+        String Interest = request.getParameter("interest");
+        String Years = request.getParameter("years");
+        String Compounding = request.getParameter("compounding");
 
+        String error;
+
+        if (Principal == null || Interest == null || Years == null || Compounding == null){
+            error = "Un des champs est vide. Remplisser tout les champs.";
+            request.setAttribute("error",error);
+        }else{
+
+        }
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
 
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,16 +8,16 @@
 
 <form action="/InterestRateCalculator" method="post">
     <label for="principal">Principal: </label>
-    <input id="principal" type="number" min="1">
+    <input id="principal" type="number" min="1" name="principal">
 
     <label for="interest">Taux d'intérêt: </label>
-    <input id="interest" type="number" min="1">
+    <input id="interest" type="number" min="1" name="interest">
 
     <label for="years">Nombre d'année: </label>
-    <input id="years" type="number" min="1">
+    <input id="years" type="number" min="1" name="years">
 
     <label for="compounding">Période de composition: </label>
-    <input id="compounding" type="number" min="1">
+    <input id="compounding" type="number" min="1" name="compounding">
 
     <button type="submit">Calculer</button>
 </form>
